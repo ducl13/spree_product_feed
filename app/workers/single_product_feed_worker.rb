13e1bea0abfe9d14ffe0d37854141b0c5ac2240b
umairr.azam <omairrr.azam@gmail.com>
2024-10-07 16:59:36 +0500

    feat: xml changes with product index

--- app/workers/single_product_feed_worker.rb
+++ app/workers/single_product_feed_worker.rb
@@ -2,12 +2,12 @@ class SingleProductFeedWorker
   include Sidekiq::Worker
   sidekiq_options queue: :xml_product
 
-  def perform(url_options, current_store_id, current_currency, product_id, file_name, channel_string, doc_string, batch_size, batch_index, last_product_id, last_xml_product_id)
+  def perform(url_options, current_store_id, current_currency, product_id, file_name, channel_string, doc_string, batch_size, batch_index, last_product_id, last_xml_product_id, product_index)
     last_xml_product = Spree::XmlProduct.find_by(product_id: last_xml_product_id)
     current_store = Spree::Store.find current_store_id
     product = Spree::Product.find_by(id: product_id)
     if product.is_in_hide_from_nav_taxon?
-      if (batch_size == (batch_index + 1) && last_xml_product_id == product_id)
+      if (batch_size == (batch_index + 1) && product_index == 99)
         last_xml_product.update(status: "processed")
         append_rss_end_tag(file_name, batch_size, batch_index, product_id, last_xml_product)
       end
@@ -18,7 +18,7 @@ class SingleProductFeedWorker
         product.variants.each do |variant|
           if variant.show_in_product_feed?
             item = Renderer::Products.create_node("item")
-            Renderer::Products.complex_product(url_options, current_store, current_currency, item, product, variant, last_xml_product)
+            Renderer::Products.complex_product(url_options, current_store, current_currency, item, product, variant, last_xml_product,batch_size, batch_index,product_index)
             file = File.open(Rails.root.join('tmp', file_name), 'a+')
             if file.include?('</channel>')
               debugger
@@ -30,14 +30,14 @@ class SingleProductFeedWorker
               file.write(item.to_s) 
             end
 
-            if batch_size == (batch_index+1) && last_xml_product_id == product_id
+            if batch_size == (batch_index+1) && product_index == 99
               append_rss_end_tag(file_name, batch_size, batch_index, product_id, last_xml_product)
              end
           end
         end
       else
         item = Renderer::Products.create_node("item")
-        Renderer::Products.basic_product(url_options, current_store, current_currency, item, product, last_xml_product)
+        Renderer::Products.basic_product(url_options, current_store, current_currency, item, product, last_xml_product, batch_size, batch_index,product_index)
         file = File.open(Rails.root.join('tmp', file_name), 'a+') # Use 'w' to open for writing
         if file.include?('</channel>')
           data =  "\n</channel>\n</rss>"
@@ -47,13 +47,13 @@ class SingleProductFeedWorker
         else
           file.write(item.to_s) 
         end
-        if batch_size == (batch_index+1) && last_xml_product_id == product_id
+        if batch_size == (batch_index+1) && product_index == 99
           last_xml_product.update(status: "processed")
           append_rss_end_tag(file_name, batch_size, batch_index, last_product_id, last_xml_product)
          end
       end
     else
-      if batch_size == (batch_index+1) && last_xml_product_id == product_id
+      if batch_size == (batch_index+1) && product_index == 99
         last_xml_product.update(status: "processed")
         append_rss_end_tag(file_name, batch_size, batch_index, last_product_id, last_xml_product)
      end
@@ -65,13 +65,11 @@ class SingleProductFeedWorker
 
   def append_rss_end_tag(file_name, batch_size, batch_index, last_product_id, last_xml_product)
     sleep(10)
-    if last_product_id == last_xml_product.product_id && last_xml_product.status == "processed"
       file = File.open(Rails.root.join('tmp', file_name), 'a+')
       file.seek(0, IO::SEEK_END)
       data =  "\n</channel>\n</rss>"
       file.write(data)
       file.close
       last_xml_product.update(status: nil)
-    end
   end
 end
