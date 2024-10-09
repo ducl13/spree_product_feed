cb34ce89923bee89ea8c1625491e32d41991ff8a
umairr.azam <omairrr.azam@gmail.com>
2024-10-09 15:35:46 +0500

    updated code

--- app/services/renderer/products.rb
+++ app/services/renderer/products.rb
@@ -80,7 +80,7 @@ class Renderer::Products
         <g:#{product.unique_identifier_type}>#{product.unique_identifier}</g:#{product.unique_identifier_type}>
         <g:sku>#{product.sku}</g:sku>
         <g:product_type>#{google_product_type(product)}</g:product_type>
-        #{product.product_properties.map { |pp| "<product_feed_property><name>#{pp.property.name.downcase}</name><value>#{pp.value}</value></product_feed_property>" if pp.property.presentation.downcase == 'product_feed' }.join("\n")}
+        <product_properties>#{product.product_properties.map { |pp| "<product_feed_property><name>#{pp.property.name.downcase}</name><value>#{pp.value}</value></product_feed_property>" if pp.property.presentation.downcase == 'product_feed' }.join("\n")}</product_properties>
       </item>
     XML
   end
@@ -91,59 +91,59 @@ class Renderer::Products
       product_url = base_url + product_url(url_options, product) + "?variant=" + variant.id.to_s
 
     <<~XML
-      <item>
-        <g:id>#{(current_store.id.to_s + "-" + product.id.to_s + "-" + variant.id.to_s).downcase}</g:id>
-        #{product.property("g:title").present? ? "" : "<g:title>#{current_store.name + ' ' + product.name + ' ' + options_xml_hash.first.presentation}</g:title>"}
-        <g:condition>new</g:condition>
-        #{ 
-          if product.property("g:description").present?
-            ""
-          else
-            if product.respond_to?(:short_description) && product.short_description.present?
-              "<g:description>#{product.short_description}</g:description>"
-            elsif product.description.present?
-              "<g:description>#{product.description}</g:description>"
-            else
-              "<g:description>#{product.meta_description}</g:description>"
-            end
-          end
-        }
-        <g:link>#{product_url}</g:link>
-        #{ 
-          (product.images.to_a + variant.images.to_a).map.with_index do |image, index|
-            if index == 0
-              "<g:image_link>#{image.my_cf_image_url(:large)}</g:image_link>"
-            elsif !product.images.blank? && !product.images.include?(image)
-              "<g:additional_image_link>#{image.my_cf_image_url(:large)}</g:additional_image_link>"
-            end
-          end.join("\n")
-        }
-        <g:availability>#{product.in_stock? ? "in stock" : "out of stock"}</g:availability>
-        #{ 
-          if variant.on_sale?
-            "<g:price>#{sprintf('%.2f', variant.original_price)} #{current_currency}</g:price>\n<g:sale_price>#{sprintf('%.2f', variant.price)} #{current_currency}</g:sale_price>"
-          else
-            "<g:price>#{sprintf('%.2f', variant.original_price)} #{current_currency}</g:price>"
-          end
-        }
-        <g:shipping_weight>#{sprintf('%.2f', variant.weight)} lb</g:shipping_weight>
-        <g:brand>#{current_store.name}</g:brand>
-        <g:#{variant.unique_identifier_type}>#{product.unique_identifier}</g:#{variant.unique_identifier_type}>
-        <g:sku>#{variant.sku}</g:sku>
-        <g:item_group_id>#{(current_store.id.to_s + "-" + product.id.to_s).downcase}</g:item_group_id>
-        <g:product_type>#{google_product_type(product)}</g:product_type>
-        <g:custom_label_0>#{product.feed_category}</g:custom_label_0>
-        #{ 
-          options_xml_hash.each_with_index.map do |ops, index|
-            if ops.option_type[:name] == "color"
-              "<g:#{ops.option_type.presentation.downcase.parameterize(separator: '_')}>#{ops.name}</g>\n<g:custom_label_#{index+1}>#{ops.name}</g:custom_label_#{index+1}>" unless (index+1) > 5
-            else
-              "<g:size>#{ops.presentation}</g:size>\n<g:custom_label_#{index+1}>#{ops.presentation}</g:custom_label_#{index+1}>" unless (index+1) > 5
-            end
-          end.join("\n")
-        }
-        #{product.product_properties.map { |pp| "<product_feed_property><name>#{pp.property.name.downcase}</name><value>#{pp.value}</value></product_feed_property>" if pp.property.presentation.downcase == 'product_feed' }.join("\n")}
-      </item>
+          <item>
+            <g:id>#{(current_store.id.to_s + "-" + product.id.to_s + "-" + variant.id.to_s).downcase}</g:id>
+            #{product.property("g:title").present? ? "" : "<g:title>#{current_store.name + ' ' + product.name + ' ' + options_xml_hash.first.presentation}</g:title>"}
+            <g:condition>new</g:condition>
+            #{ 
+              if product.property("g:description").present?
+                ""
+              else
+                if product.respond_to?(:short_description) && product.short_description.present?
+                  "<g:description>#{product.short_description}</g:description>"
+                elsif product.description.present?
+                  "<g:description>#{product.description}</g:description>"
+                else
+                  "<g:description>#{product.meta_description}</g:description>"
+                end
+              end
+            }
+            <g:link>#{product_url}</g:link>
+            #{ 
+              (product.images.to_a + variant.images.to_a).map.with_index do |image, index|
+                if index == 0
+                  "<g:image_link>#{image.my_cf_image_url(:large)}</g:image_link>"
+                elsif !product.images.blank? && !product.images.include?(image)
+                  "<g:additional_image_link>#{image.my_cf_image_url(:large)}</g:additional_image_link>"
+                end
+              end.join("\n")
+            }
+            <g:availability>#{product.in_stock? ? "in stock" : "out of stock"}</g:availability>
+            #{ 
+              if variant.on_sale?
+                "<g:price>#{sprintf('%.2f', variant.original_price)} #{current_currency}</g:price>\n<g:sale_price>#{sprintf('%.2f', variant.price)} #{current_currency}</g:sale_price>"
+              else
+                "<g:price>#{sprintf('%.2f', variant.original_price)} #{current_currency}</g:price>"
+              end
+            }
+            <g:shipping_weight>#{sprintf('%.2f', variant.weight)} lb</g:shipping_weight>
+            <g:brand>#{current_store.name}</g:brand>
+            <g:#{variant.unique_identifier_type}>#{product.unique_identifier}</g:#{variant.unique_identifier_type}>
+            <g:sku>#{variant.sku}</g:sku>
+            <g:item_group_id>#{(current_store.id.to_s + "-" + product.id.to_s).downcase}</g:item_group_id>
+            <g:product_type>#{google_product_type(product)}</g:product_type>
+            <g:custom_label_0>#{product.feed_category}</g:custom_label_0>
+            #{ 
+              options_xml_hash.each_with_index.map do |ops, index|
+                if ops.option_type[:name] == "color"
+                  "<g:#{ops.option_type.presentation.downcase.parameterize(separator: '_')}>#{ops.name}</g>\n<g:custom_label_#{index+1}>#{ops.name}</g:custom_label_#{index+1}>" unless (index+1) > 5
+                else
+                  "<g:size>#{ops.presentation}</g:size>\n  <g:custom_label_#{index+1}>#{ops.presentation}</g:custom_label_#{index+1}>" unless (index+1) > 5
+                end
+              end.join("\n")
+            }
+            <product_properties>#{product.product_properties.map { |pp| "<product_feed_property><name>#{pp.property.name.downcase}</name><value>#{pp.value}</value></product_feed_property>" if pp.property.presentation.downcase == 'product_feed' }.join("\n")}</product_properties>
+          </item>
     XML
   end
 
